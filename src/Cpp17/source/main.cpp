ac6f742cf6a50f7a9a224bb7b839a4d2ce2923af
adrian925492 <adrian.podsiadlowski@gmail.com>
2020-11-04 17:07:47 +0000

    ways of init

--- src/Cpp17/source/main.cpp
+++ src/Cpp17/source/main.cpp
@@ -1,9 +1,10 @@
 #include "new_features_cpp17/structural_binding.hpp"
 #include "new_features_cpp17/variable_range_limitation.hpp"
-
+#include "new_features_cpp17/way_of_initialization.hpp"
 
 int main(void)
 {
     structural_binding_example();
     varaible_range_limitation_example();
+    way_of_initialization_example();
 }
