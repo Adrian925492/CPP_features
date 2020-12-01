a46a745204900b1fd2a721cf18d0c828618d7a02
adrian925492 <adrian.podsiadlowski@gmail.com>
2020-12-01 18:12:49 +0000

    sort and filter

--- src/Cpp17/source/algorithms/algorithms.cpp
+++ src/Cpp17/source/algorithms/algorithms.cpp
@@ -8,8 +8,17 @@
 #include <algorithm>
 #include <iterator>
 #include <map>
+#include <random>
 
 using namespace std;
+
+//Simple helper function - print the vector content
+static void print(const vector<int> &v)
+{
+    copy(v.begin(), v.end(), ostream_iterator<int>{cout, ", "});
+    cout << endl;
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // RECEPIE 1: Copy algorithm example
@@ -68,12 +77,111 @@ void copy_example()
 // RECEPIE 2: Sort algorithm example
 
 /* 
-
+The recepie presents sorting algorithms examples. Depending on type of container, there are
+several algorithms implemented (choosing sort, merging sort, insert sort etc...). Compilse chooses 
+best autoamtically.
 */
 
 void sort_example()
 {
     cout << "Sort example! \n\n";
+
+    vector<int> v{1,2,3,4,5,6,7,8,9};   //source vetor
+
+    random_device rd;
+    mt19937 g {rd()};                   // Random generator init
+
+    cout << "Input container: ";
+    print(v);
+    cout << "Sorted: " << is_sorted(v.begin(), v.end()) << endl;
+
+    shuffle(v.begin(), v.end(), g);     //Shuffle vector
+
+    cout << "\nShuffled vector: ";
+    print(v);
+    cout << "Sorted: " << is_sorted(v.begin(), v.end()) << endl;
+
+    // Sort
+    sort(v.begin(), v.end());
+    cout << "\nSort() called: Sorted: " << is_sorted(v.begin(), v.end()) << "vector: ";
+    print(v);
+
+    // Partition
+    shuffle(v.begin(), v.end(), g);    // Shuffle vector
+    partition(v.begin(), v.end(), [](int e){return e<5;});   //Partition - all less than 5 elements on the beginning
+    
+    cout << "\nPartitioned vector: ";
+    print(v);
+
+    // Partial sort
+    shuffle(v.begin(), v.end(), g);    // Shuffle vector
+    auto middle(next(v.begin(), int(v.size() / 2)));    //Find middle element of a vector
+    partial_sort(v.begin(), middle, v.end());
+
+    cout << "\nPartial sorted (to the middle) vector: ";
+    print(v);
+
+    //Sort in opposite way - by passing compare fcn
+    shuffle(v.begin(), v.end(), g);    // Shuffle vector
+    sort(v.begin(), v.end(), [](int& lhs, int& rhs){return rhs < lhs;});
+    cout << "\nOpposite sorted vector: ";
+    print(v);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// RECEPIE 3: Filter (erase) example
+
+/* 
+The recepie presents filter algorithms examples. The algorithms mahe removin or copying
+filtered elements from input to output range.
+*/
+
+void filter_example()
+{
+    cout << "Filter example! \n\n";
+
+    vector<int> v{1,2,3,4,5,6,7,8,9};   //source vetor
+
+    cout << "Input container: ";
+    print(v);
+
+    // Remove all 2-s:
+    const auto new_end(remove(v.begin(), v.end(), 2));  //Not erases, just moves searched element to the end of container
+    v.erase(new_end, v.end());
+
+    cout << "\nErased all 2s vector: ";
+    print(v);
+
+    // Now erase all odd numbers
+    auto odd ([](int i){return i%2 != 0;});
+    const auto new_end2(remove_if(v.begin(), v.end(), odd));
+    v.erase(new_end2, v.end());
+
+    cout << "\nErased all odd values in vector: ";
+    print(v);
+
+    // Replace all 4-s to 16
+    replace(v.begin(), v.end(), 4, 16);
+
+    cout << "\nReplaced all 4 values to 16 in vector: ";
+    print(v);
+
+    // Copy only odd numbers
+    vector<int> w{1,2,3,4,5,6,7,8,9};   //source vetor
+    vector<int> w1;
+    vector<int> w2;
+
+    auto odd_val ( [](int i){ return i % 2 != 0;});
+    auto even_val ( [](int i){ return i % 2 == 0;});
+
+    remove_copy_if(w.begin(), w.end(), back_inserter(w1), odd_val);
+    copy_if(w.begin(), w.end(), back_inserter(w2), even_val);
+
+    cout << "Copied only even values in 2 ways: ";
+    print(w1);
+    cout << "\n";
+    print(w2);
+    cout << endl;
 }
 
 void algorithms_example()
@@ -83,5 +191,7 @@ void algorithms_example()
     copy_example();
 
     sort_example();
+
+    filter_example();
 
 }
