3e3e9ce0b43042b95fffde60935a52369007f768
adrian925492 <adrian.podsiadlowski@gmail.com>
2021-01-27 19:05:14 +0100

    parallel_mandelbort

--- src/Cpp17/source/parallel_processing/parallell_processing.cpp
+++ src/Cpp17/source/parallel_processing/parallell_processing.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <numeric>
+#include <iterator>
 #include <chrono>
 #include <thread>
 #include <execution>
@@ -13,6 +15,7 @@
 #include <future>
 #include <queue>
 #include <condition_variable>
+#include <complex>
 #include "parallell_processing.h"
 
 using namespace std;
@@ -486,6 +489,81 @@ void extended_producer_consumer_example()
     cout << "Finish " << endl << endl;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+// RECEPIE 11: Mandlebort parallell example
+
+using cplx = complex<double>;
+
+// Scaller - will convert coordinates of complex types to mandelbort coordinates
+static auto scaler(int min_from, int max_from, double min_to, double max_to)
+{
+    const int w_from {max_from - min_from};
+    const double w_to {max_to - min_to};
+    const int mid_from {(max_from - min_from) / 2 + min_from};
+    const double mid_to {(max_to - min_to) / 2.0 + min_to};
+    return [=](int from) {
+        return double(from - mid_from) / w_from * w_to + mid_to;
+    };
+}
+
+template<typename A, typename B>
+static auto scaled_cmplx(A scaler_x, B scaler_y)
+{
+    return [=](int x, int y) {
+        return cplx{scaler_x(x), scaler_y(y)};
+    };
+}
+
+static auto mandelbort_iterations(cplx c)
+{
+    cplx z {};
+    size_t iterations = 0;
+    const size_t max_iterations = 1000;
+    while(abs(z) < 2 && iterations < max_iterations) {
+        ++iterations;
+        z = pow(z, 2) + c;
+    }
+    return iterations;
+}
+
+void parallell_mandelbort_example()
+{
+    cout << "Parallell manderbolt example\n\n";
+
+    //Scalling section - we will keep results in linear vector, so we need to scale manderbold
+    // space to the vector space
+    const size_t w = 100;
+    const size_t h = 40;
+    auto scale (scaled_cmplx(
+        scaler(0, w, -2.0, 1.0),
+        scaler(0, h, -1.0, 1.0)
+    ));
+
+    auto i_to_xy ([=](int i){return scale(i%w, i/w);});
+
+    auto to_iteration_count ([=](int i) {
+        return async(launch::async, mandelbort_iterations, i_to_xy(i));
+    });
+
+    // Here the manderbolt result will be stored in v.
+    vector<int> v (w * h);
+    vector<future<size_t>> r (w * h);
+    iota(begin(v), end(v), 0);
+    transform (begin(v), end(v), begin(r), to_iteration_count);
+
+    // Printing section
+    auto binfunc ([w, n{0}] (auto output_it, future<size_t>& x) mutable {
+        *++output_it = (x.get() > 50 ? '*' : ' ');
+        if (++n % w == 0) {++output_it = '\n';}
+        return output_it;
+    });
+
+    accumulate(r.begin(), r.end(), ostream_iterator<char>{cout}, binfunc);
+
+    cout << endl << endl;
+}
+
+
 void parallell_processing_example()
 {
     using_policies();
@@ -507,4 +585,6 @@ void parallell_processing_example()
     condition_variable_example();
 
     extended_producer_consumer_example();
+
+    parallell_mandelbort_example();
 }
