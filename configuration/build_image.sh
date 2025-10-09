9323a8de8bb92a5e8d959709e14959d3cbe39aff
APodsiadlowski <adrian.podsiadlowski@adtran.com>
2025-10-09 20:15:15 +0200

    Modify Dockerfile and scripts to support Cpp20

--- configuration/build_image.sh
+++ configuration/build_image.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -f=`pwd`/Dockerfile -t=adrian9254/gcc_image .
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Build the Docker image using absolute paths
+docker build -f="${SCRIPT_DIR}/Dockerfile" -t=adrian9254/gcc_20_image "${SCRIPT_DIR}"
--- configuration/run_container.sh
+++ configuration/run_container.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Get the parent directory (repository root)
+REPO_DIR="$(dirname "${SCRIPT_DIR}")"
+
+# Run the container with the repository mounted
 docker run \
 	-it \
-	--volume `pwd`/..:/repo \
+	--volume "${REPO_DIR}":/repo \
 	--rm \
-	--name=gcc_container \
+	--name=gcc_20_container \
 	--net=bridge \
 	--user=root \
-	adrian9254/gcc_image
+	adrian9254/gcc_20_image
